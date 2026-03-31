27b2a235bad8296f840c1a854abfeb48a24b0daa
Bonbonkocher <johnnydepp1987@hotmail.de>
2026-03-31 23:40:37 +0200

    Delete git-hilf-programm.bat

--- git-hilf-programm.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: Hier den Namen der Datei anpassen, falls du sie umbenennst
-set scriptName=git-hilf-programm.ps1
-
-:: Startet PowerShell mit dem oben definierten Namen im selben Ordner (%~dp0)
-powershell -NoExit -ExecutionPolicy Bypass -File "%~dp0%scriptName%"
